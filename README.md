52f1c573e9ab3c5d2ea2b98615401729ac147396
Volter9 <volter925@gmail.com>
2014-11-24 08:40:18 -0800

    Initiate Form Preview

--- README.md
+++ README.md
@@ -1,4 +1,94 @@
-jQuery-Form-Preview
-===================
+# jQuery Form Preview
 
 Need to visualize some values from form elements? No problem, Form Preview to the rescue!
+This plugin for jQuery is suitable for visualizing form input into HTML elements. Want to see some examples? Sure, go for it:
+
+* [Hello, World!](http://jsfiddle.net/volter9/5gykse6u/)
+* [Show me your article card](http://jsfiddle.net/volter9/L1x151zf/)
+* Few more to come...
+
+## Features
+
+What can do jQuery Form Preview? Hmm, it can do a lot of things, but everything is depends on your imagination.
+
+## Usage
+
+This plugin is really easy to use.
+
+### $(element).preview(options)
+
+What we've got here? This is the core of jQuery plugin. Here's the syntax of this function:
+
+```js
+$('#id').preview({
+	form   : 'article > form',
+	fields : 'input, select',
+	event  : 'change',
+	init   : function (preview) {
+		// Code related to preview before setup should be executed here
+	},
+})
+```
+
+So what's those arguments about?
+
+* `#id` - Desired element which is your target, basically, the ~~div~~ any element that contains previewed markup. See **Markup** section
+* `form: 'article > form'` - Form which is going to be used, only one form allowed per preview, **required option**.
+* `fields: 'input, select',` - Which form elements you want in form to be *parsed*. Default: `'input, textarea, select'`.
+* `event: 'change'` - Which event would trigger preview. Default: `'input'`.
+* `init: function (preview) {}` - Initialization callback, add processors if you need to on different fields.
+
+### Preview.addProcessor(name, callback)
+
+Need to process some values before previewing the data? Welcome to the processing 101 in Form Preview!
+
+First argument is the `name` of the field, second argument is `callback` which will process the input. Example of `callback` that will capitalize `title` field before outputting:
+
+```js
+preview.addProcessor('title', function (name, value, input) {
+	return value.toUpperCase();
+});
+```
+
+`callback` takes three parameters:
+
+* `name` - The same as first argument in `addProcessor` method. That's in case if you need the name of the field, but you want to avoid repetition and follow DRY principle.
+* `value` - Value of `name` field before passed to you before previewing.
+* `input` - Plain object of other values, in case if your value should depend on other field value.
+
+### Markup
+
+In this version, you need to specify markup for preview to be filled in HTML.
+jQuery Form Preview plugin supports three data-* attributes to help you build previews.
+
+In the `#id` element you should put desired HTML with following attributes:
+
+* `data-preview="field"` - Field which will be used to fill information inside of the element (`.html()` is used for non attribute fields). **Required attribute** in order to make work other data-* attributes.
+* `data-attribute="attribute"` - Specify attribute to fill values in attribute instead of content. **Only one attribute** is supported yet.
+* `data-pattern="Peter %s pattern %s"` - If you need not just output, but also a label in front of it, or you want to use the value twice or three times, then this it is a job for *super-pattern-man*. Use `%s` to insert your value. data-pattern **works with** data-preview as well as with data-attribute.
+
+That's it! Thank you for attention, fellows!
+
+## License
+
+Hmm, usually in this section there's a license. Here's MIT license for you:
+
+Copyright (c) 2014 volter9
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ form-preview.js
@@ -0,0 +1,226 @@
+/**
+ * jQuery data form preview plugin.
+ *
+ * @author volter9
+ * @copyright volter9 (c) 2014
+ * @license MIT
+ * @website https://github.com/Volter9/jQuery-Form-Preview
+ * @version 0.1
+ */
+(function ($) {	
+	/**
+	 * Hmm, I'm not sure, but maybe somebody already using my functions names.
+	 * Let's see how it will work out.
+	 */
+	if (typeof $.fn.preview === 'function' ||
+		typeof $.fn.extract === 'function') {
+		throw new Error('$.fn.preview or $.fn.extract is already defined!');
+	}
+	
+	/**
+	 * Preview constructor.
+	 * 
+	 * @param {Object} options
+	 */
+	function Preview (options) {
+		this.target   = options.target;
+		this.form     = $(options.form).first();
+		this.fields   = this.form.find(options.fields);
+		this.event    = options.event;
+		
+		this.initiated  = false;
+		this.data       = {};
+		this.processors = {};
+		
+		options.init(this);
+		
+		this.setup();
+	}
+	
+	/**
+	 * @var {string} version - Version of jQuery Form Preview plugin
+	 */
+	Preview.prototype.version = 'v0.1';
+	
+	Preview.prototype.constructor = Preview;
+	
+	/**
+	 * Setup method.
+	 * Sets all events and assemble preview.
+	 *
+	 * Note: _this is for Preview object, $this is for jQuery routines
+	 */
+	Preview.prototype.setup = function () {
+		if (this.initiated) {
+			return;
+		}
+		
+		var _this = this;
+		
+		this.fields.on(this.event, function () {
+			var $this = $(this);
+			
+			var data = $this.extract();
+			
+			if (!data) {
+				return;
+			}
+						
+			data.value = _this.process(data.name, data.value);
+			
+			_this.update(data.name, data.value);
+			_this.preview(data.name);
+		});
+		
+		this.collect();
+		this.preview();
+	};
+	
+	/**
+	 * Process the given name and value.
+	 * 
+	 * @param {String} name
+	 * @param {String} value
+	 */
+	Preview.prototype.process = function (name, value) {
+		var oldValue  = value;
+		var processor = this.processors[name];
+		
+		if (processor) {
+			value = processor(name, value, this.data);
+		}
+		
+		if (value === undefined) {
+			value = oldValue;
+		}
+		
+		return value;
+	};
+	
+	/**
+	 * Following method collects data from input.
+	 */
+	Preview.prototype.collect = function () {
+		var _this = this;
+		
+		this.fields.each(function () {
+			var $this = $(this);
+			
+			var data = $this.extract();
+			
+			data.value = _this.process(data.name, data.value);
+			
+			if (data) {
+				_this.update(data.name, data.value);
+			}
+		});
+	};
+	
+	/**
+	 * Updating collected data
+	 * 
+	 * @param {String} name
+	 * @param {String} value
+	 */
+	Preview.prototype.update = function (name, value) {
+		if ( !name ) {
+			return false;
+		}
+		
+		this.data[name] = value;
+	};
+	
+	/**
+	 * Preview 
+	 * 
+	 * @param {String} field
+	 */
+	Preview.prototype.preview = function (field) {
+		if ( !field ) {
+			var _this = this;
+			
+			$.each(this.data, function (property) {
+				_this.preview(property);
+			});
+			
+			return;
+		}
+		
+		var value = this.data[field];
+		var element = this.target.find('[data-preview=' + field + ']');
+						
+		var attribute = element.attr('data-attr'),
+			pattern   = element.attr('data-pattern');
+		
+		if (pattern) {
+			value = pattern.replace(/%s/g, value);
+		}
+		
+		if (!attribute) {
+			element.html(value);
+		}
+		else {
+			element.attr(attribute, value);
+		}
+	};
+	
+	/**
+	 * Add processor for specific field.
+	 * 
+	 * @param {String} name
+	 * @return {void|Boolean}
+	 */
+	Preview.prototype.addProcessor = function (name, callback, force) {
+		if ( this.processors[name] && !force ) {
+			return false;
+		}
+		
+		this.processors[name] = callback;
+	};
+	
+	/**
+	 * Utility function for form elements.
+	 * Use it to extract object with name and value properties form element.
+	 * 
+	 * @return {Object|Boolean}
+	 */
+	$.fn.extract = function () {
+		var attribute = this.attr('name');
+		
+		if ( !attribute ) {
+			return false;
+		}
+		
+		return {
+			name  : attribute,
+			value : this.val()
+		};
+	};
+	
+	/**
+	 * Preview form.
+	 * Required fields: form
+	 * 
+	 * @param {Object} options - list of options
+	 * @return {Preview}
+	 */
+	$.fn.preview = function (options) {
+		if ( typeof options.form !== 'string' ) {
+			throw new TypeError('Option "form" must be a string. Given: ' + typeof options.form);
+		}
+		
+		var byDefault = {
+			fields   : 'input, textarea, select',
+			event    : 'input',
+			init     : function () {}
+		};
+		
+		options = $.extend(byDefault, options);
+		options.target = this;
+		
+		var preview = new Preview(options);
+		
+		return preview;
+	};
+	
+})(jQuery);
